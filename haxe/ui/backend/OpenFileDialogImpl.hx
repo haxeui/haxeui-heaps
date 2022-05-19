90951fe6f5ae5cd9e0f661f05bc36e86cc544d5f
Ian Harrigan <ianharrigan@hotmail.com>
2022-05-19 11:12:00 +0200

    refactor file dialogs (SelectFile -> OpenFile)

---
+++ haxe/ui/backend/OpenFileDialogImpl.hx
@@ -0,0 +1,72 @@
+package haxe.ui.backend;
+
+import haxe.ui.containers.dialogs.Dialogs.SelectedFileInfo;
+import haxe.ui.core.Platform;
+
+using StringTools;
+
+class OpenFileDialogImpl extends OpenFileDialogBase {
+    #if hl
+    
+    public override function show() {
+        if (Platform.instance.isWindows) {
+            var title = options.title;
+            if (title == null) {
+                title = "Open File";
+            }
+            var file = hl.UI.loadFile({title: title });
+            if (file != null) {
+                var infos:Array<SelectedFileInfo> = [];
+                infos.push({
+                    name: haxe.io.Path.withoutDirectory(file),
+                    fullPath: file,
+                    isBinary: false
+                });
+                
+                if (options.readContents == true) {
+                    for (info in infos) {
+                        if (options.readAsBinary) {
+                            info.isBinary = true;
+                            info.bytes = sys.io.File.getBytes(info.fullPath);
+                        } else {
+                            info.isBinary = false;
+                            info.text = sys.io.File.getContent(info.fullPath);
+                        }
+                    }
+                }
+                
+                dialogConfirmed(infos);
+            } else {
+                dialogCancelled();
+            }
+        } else {
+            super.show();
+        }
+    }
+    
+    #elseif js
+
+    private var _fileSelector:haxe.ui.backend.heaps.FileSelector = new haxe.ui.backend.heaps.FileSelector();
+    
+    public override function show() {
+        var readMode = haxe.ui.backend.heaps.FileSelector.ReadMode.None;
+        if (options.readContents == true) {
+            if (options.readAsBinary == false) {
+                readMode = haxe.ui.backend.heaps.FileSelector.ReadMode.Text;
+            } else {
+                readMode = haxe.ui.backend.heaps.FileSelector.ReadMode.Binary;
+            }
+        }
+        _fileSelector.selectFile(onFileSelected, readMode, options.multiple, options.extensions);
+    }
+    
+    private function onFileSelected(cancelled:Bool, files:Array<SelectedFileInfo>) {
+        if (cancelled == false) {
+            dialogConfirmed(files);
+        } else {
+            dialogCancelled();
+        }
+    }
+    
+    #end
+}
